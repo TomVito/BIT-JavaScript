2179a18e9243ccb26136925d1c48b8063e676374
TomVito <t.vitonis@gmail.com>
2021-07-01 21:21:42 +0300

    console.log and trigger button click on enter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,30 +29,69 @@ function ikrepseli() {
   if (kiekis % 10 == 1) {
     zodis = "prekę";
   }
-
   if ((kiekis > 10 && kiekis < 20) || kiekis % 10 == 0) {
+    zodis = "prekių";
+  }
+  if ((kiekis % 100 > 10 && kiekis % 100 < 20) || kiekis % 10 == 0) {
     zodis = "prekių";
   }
 
   var zinute =
     "Jūs sėkmingai pridėjote " + kiekis + " " + zodis + " į savo krepšelį!";
 
-  if ((kiekis % 100 > 10 && kiekis % 100 < 20) || kiekis % 10 == 0) {
-    zodis = "prekių";
-  }
   if (kiekis < 1) zinute = "Pasirinktas per mažas kiekis";
-
   if (kiekis > 100)
-    zinute = "Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas.";
+    zinute = "Jūsų pasirinktas prekių kiekis yra didesnis nei leistinas";
 
   document.getElementById("messages").innerHTML = zinute;
 }
 
+// click on enter
+
+var input = document.getElementById("kiekis");
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("kvietejas").click();
+  }
+});
+
 // reset button
 
-document.getElementById("result").addEventListener("click", clearResult);
+document.getElementById("result").addEventListener("click", clear);
 
-function clearResult() {
+function clear() {
   document.getElementById("kiekis").value = "";
   document.getElementById("messages").innerHTML = "";
+}
+
+//masyvas
+
+const data = [
+  {
+    Vardas: "Vladas",
+    Pavardė: "Vladanas",
+    Gimimo_metai: "1969-06-09",
+    Tautybė: "TSRS",
+  },
+  {
+    Vardas: "Antanas",
+    Pavardė: "Vlantanas",
+    Gimimo_metai: "1954-01-03",
+    Tautybė: "Kazachstanian",
+  },
+  {
+    Vardas: "Stasas",
+    Pavardė: "Basas",
+    Gimimo_metai: "1980-09-14",
+    Tautybė: "Yugoslavyan",
+  },
+];
+
+for (let index in data) {
+  let row = "";
+  for (let text in data[index]) {
+    row += data[index][text] + " ";
+  }
+  console.log(row);
 }
